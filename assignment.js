199f96c7f9ac645a5976542132bab118f4039cb3
tejalrathod123 <rathodtejal746@gmail.com>
2022-10-04 17:47:30 +0530

    First program execution in JS
    First program execution in JS

---
+++ assignment.js
@@ -0,0 +1,6 @@
+console.log("Tejal Harishchandra Rathod");
+console.log("My hobbies are 1-Reading books 2-cooking 3-Travelling");
+console.info("My Strength- positive attitude, Hardworking, Honest.");
+console.warn("My weakness- english communication, Overthinker, impatiance.");
+console.log(30+50);
+console.log(49*479);
