0f5694b77b75bf2e1ae6da90096b6163574d376f
tejalrathod123 <rathodtejal746@gmail.com>
2022-10-04 17:15:32 +0530

    js learning
    js learning

---
+++ demo.js
@@ -0,0 +1,10 @@
+//This is my first program
+//Now I know the two ways to run pragram */
+console.log("hello");
+console.log("my name is tejal rathod");
+//info means information
+console.info("this is info"); 
+//warn means warning
+console.warn("i am a good girl");
+console.error("opps failed toexecute");
+console.trace
