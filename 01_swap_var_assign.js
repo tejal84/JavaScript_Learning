2c03000f09f40d4f40ebe9b81a519eeddc524a30
tejalrathod123 <rathodtejal746@gmail.com>
2022-10-07 06:39:44 +0530

    js assignments
    js assignments

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,17 @@
+var sweety;
+var cutie;
+console.log("sweety,cutie"); //before swaping
+console.log("cutie,sweety"); //after swaping
+
+var num1="100";
+var num2="200";
+var num3="300";
+console.log("before swaping");
+console.log(num1,num2,num3);
+ 
+var temp =num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("after swaping")
+console.log(num1,num2,num3);
---
+++ 01_variable_assign.js
@@ -0,0 +1,25 @@
+var myFullName;
+console.log(myFullName);
+myFullName = "Tejal  Rathod"
+console.log(myFullName); //before initialize
+var myFullName;
+myFullName = "Tejal Harishchandra Rathod"
+console.log(myFullName); //after initialize
+var myLove;
+myLove = "I love only js"
+console.log(myLove)
+
+var firstName = "Tejal";
+console.log(firstName);
+var lastName = "Rathod";
+console.log(lastName);
+var age = "24"
+console.log(age);
+var address = "Sangli";
+console.log(address);
+var pin_code = "415409";
+console.log(pin_code);
+var gender = "Female";
+console.log(gender);
+var isMarried = "No";
+console.log(isMarried);
