ca7375b690a9981ed365176dc067ebbbca01a44f
tejalrathod123 <rathodtejal746@gmail.com>
2022-10-07 08:51:00 +0530

    js learning
    js learning

---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+var p_name = "Prashant";
+var age = 22;
+var city = 'Pune';
+var married_status = false;
+
+console.log(typeof 31 );
+console.log(typeof false);
+console.log(typeof "Pradnya");
+console.log("================");
+console.log(typeof p_name);
+console.log(typeof age);
+console.log(typeof married_status);
+
+console.log("================");
+var my_project_name;
+console.log(my_project_name);
+console.log("================");
+console.log(typeof my_project_name);
+
+var my_properties = null;
+
+console.log(typeof my_properties);
---
+++ 03_function.js
@@ -0,0 +1,19 @@
+function tejal(money){
+    console.log(money);
+    console.log("please go in market with rs.:",money);
+    console.log("Buy some vegetables");
+    return "vegetables bag";
+
+}
+var veg =tejal(500);
+console.log(veg);
+
+function functionName(city, street, age, is_married){
+// city
+// street
+// age
+// is_married
+return true; 
+
+}
+var value= functionName("pune", "khaugalli", "24", );
