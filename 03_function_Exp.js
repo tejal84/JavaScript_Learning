0881f50b4761bbc15344f9caf67b255d258278fa
tejalrathod123 <rathodtejal746@gmail.com>
2022-10-10 08:58:19 +0530

    java script learning
    java script learning

---
+++ 03_function_Exp.js
@@ -0,0 +1,18 @@
+var test = function(arg1, arg2){
+    console.log("Function expression", arg1);
+    return "FE";
+}
+var returnValue = test(10, 20);
+console.log(returnValue);
+
+
+
+
+var square = function(value){
+console.log(value*value);
+}
+square(5);
+square(10);
+square(20);
+console.log(typeof square);
+
---
+++ 04_string.js
@@ -0,0 +1,13 @@
+var greet = "Good Morning";
+
+console.log(greet);
+
+var count = greet.length;
+console.log(count);
+
+console.log(greet.length);
+
+var sonali = greet.charAt(8);
+console.log(sonali);
+
+console.log(greet.charAt(8));
