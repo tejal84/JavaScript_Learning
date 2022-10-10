e862995a2b53e26a7922c8cc3edbcc1bc598a352
tejalrathod123 <rathodtejal746@gmail.com>
2022-10-10 16:17:44 +0530

    string learning
    string learning

---
+++ 03_string.js
@@ -0,0 +1,22 @@
+var string ="javascript is the most popular language";
+console.log(string);
+
+var len=string.length;
+console.log(len);
+
+var indexof=string.indexOf;
+console.log("find the index of character 's'");
+console.log(string.indexOf("s"));
+
+
+var indexof=string.indexOf;
+console.log("find the index of character 'lang'");
+console.log(string.indexOf("lang"));
+
+var charAt=string.charAt;
+console.log("find the last character using length property");
+console.log(string.charAt(string.length-1));
+
+var charAt=string.charAt;
+console.log("find the last character using length property");
+console.log(string.charAt(string.length-3));
