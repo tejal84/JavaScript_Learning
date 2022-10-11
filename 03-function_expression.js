aff502ef82db85ac2082aa4c758e54ba6a40b072
tejalrathod123 <rathodtejal746@gmail.com>
2022-10-11 07:30:57 +0530

    function expression
    function expression

---
+++ 03-function_expression.js
@@ -0,0 +1,38 @@
+var square = function(value){
+    console.log(value*value);
+    }
+    square(5);
+    square(6);
+    square(25);
+    square(100);
+    console.log(typeof square);
+
+ var triangleArea =function(base,height){
+var area=1/2*(base*height);
+console.log("Total Triangle area is=" +area);
+}
+triangleArea(45,34)
+  
+    
+var rectangleArea =function(length,width){
+    var rarea=length*width;
+    console.log("Total Rectangle Area is=" +rarea);
+ }
+    rectangleArea(499,917)
+var virat;
+var Amushka;
+console.log("before swaping");
+console.log("virat,Anushka");
+console.log("after swaping");
+console.log("Anushka,virat");
+
+var num1="1000";
+var num2="2000";
+console.log("before swaping");
+console.log(num1,num2);
+ var num1=num2
+num2=num1;
+console.log("after swaping");
+console.log(num1,num2,);
+
+      
