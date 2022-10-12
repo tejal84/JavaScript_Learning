5057bc6f64235132b18014feb46bc52f062aa94c
tejalrathod123 <rathodtejal746@gmail.com>
2022-10-12 08:51:30 +0530

    operator learning
    operator learning

---
+++ 05_operator.js
@@ -0,0 +1,65 @@
+console.log(10 + 10);
+console.log(10 + 100.23);
+
+var num1 = 200;
+var num2 = 400;
+var result = num1 + num2;
+console.log(`Result is : ${result}`);
+
+
+var num1 = 600;
+var num2 = 400;
+var result = num1 - num2;
+console.log(`Result is : ${result}`);
+
+var num1 = 30;
+var num2 = 20;
+var result = num1 * num2;
+console.log(`Result is : ${result}`);
+
+var num1 = 100;
+var num2 = 20;
+var result = num1 / num2;
+console.log(`Result is : ${result}`);
+
+
+console.log("=========% Module operator");
+console.log(10%2);
+
+console.log("======++ Increment=====");
+// var incr =num1+1; //
+var incr =num1++;
+var decr=num2--;
+console.log('${incr} ${decr}');
+
+var num1 =100;
+var num2 =50;
+var num1=num1 +num2;
+num1+=num2;//num1= num1+num2;
+console.log('Compound Addition operator: += ${num1}');
+
+console.log(`=======Comparison operator======`);
+var num1 = 100;
+var num2 = 50; 
+
+console.log("num1<num2 ===> ",   num1<num2);
+console.log("num1>num2 ===> ",  num1>num2);
+console.log("num1!=num2 ====> ", num1!=num2);
+
+var num3 = 50; 
+console.log("num2<num3) ===> ",  num2<num3);
+console.log("num2<=num3) ===> ",  num2<=num3);
+console.log("num2>num3=====> ", num2>num3);
+console.log("num2>=num3=====> ", num2>=num3);
+
+var num2 =50;
+var num3="50";
+console.log('****** == operator *****');
+var result = num2 ==num3; //50==50
+
+console.log("num2==num3=====> ", result);
+
+console.log("num2===num3====>", num2===num3);
+
+
+
---
+++ 05_ternary-operator.js
@@ -0,0 +1,3 @@
+var num1 = -5;
+ var result = num1 > 0 ? "Positive" : "Negative";
+ console.log(result);
