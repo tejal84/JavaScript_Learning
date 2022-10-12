b0a445df6764023f966e125b15601023f99cb891
tejalrathod123 <rathodtejal746@gmail.com>
2022-10-12 12:20:32 +0530

    string assignment
    string assignment

---
+++ 04_stringAssignment.js
@@ -0,0 +1,15 @@
+console.log("My Dream Company Name");
+var DreamCompany =" my dream company name is INFOSYS";
+console.log(DreamCompany);
+
+console.log("My Hobbies");
+var myhobby1 = "1.Reading books";
+var myhobby2 = "2.Cooking";
+var myhobby3 = "3.Travelling";
+console.log("myhobby1:",myhobby1);
+console.log("myhobby2:",myhobby2);
+console.log("myhobby3:",myhobby3);
+
+console.log("concate string in one line");
+var Hobbies=myhobby1. concat(myhobby2). concat(myhobby3);
+console.log("My Hobbies are:",Hobbies);
